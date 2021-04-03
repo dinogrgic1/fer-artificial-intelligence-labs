f6c3932c44768259e926211723689899ad2b22b2
dinogrgic1 <dino.grgic1@gmail.com>
2021-04-03 14:39:53 +0200

    Updated autograder

--- lab1py/solution.py
+++ lab1py/solution.py
@@ -1,2 +1,28 @@
+import sys
+
+
+def parse_state_space_file():
+    pass
+
+def parse_args(args):
+    alg = ss = h = optimistic = consistent = None
+    flag = None
+    for arg in args:
+        if '--' in arg:
+            flag = arg
+        else:
+            if flag == '--alg':
+                alg = arg
+            elif flag == '--ss':
+                ss = arg
+            elif flag == '--h':
+                h = arg
+            elif flag == '--check-optimistic':
+                optimistic = arg
+            elif flag == '--check-consistent':
+                consistent = arg
+    return (alg, ss, h, optimistic, consistent)    
+
 if __name__ == '__main__':
-    print('test')
+    alg, ss, h, optimistic, consistent = parse_args(sys.argv[1:])
+    print(alg, ss, h, optimistic, consistent)
